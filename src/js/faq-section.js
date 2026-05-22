a182302be9d3188f7d5be8324c478a826f8e89cb
Igor Bilovash <belovash2002@gmail.com>
2026-05-22 15:49:23 +0300

    feat: add faq section

---
+++ src/js/faq-section.js
@@ -0,0 +1,10 @@
+import Accordion from "accordion-js";
+import "accordion-js/dist/accordion.min.css";
+
+const faqAccordion = document.querySelector(".faq-accordion");
+
+new Accordion(faqAccordion, {
+    elementClass: 'faq-item',
+    triggerClass: 'faq-trigger',
+    panelClass: 'faq-panel'
+});
