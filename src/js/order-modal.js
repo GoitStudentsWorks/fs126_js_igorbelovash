904e85ce0c6895d47fc45a1678fe9375913d92a0
zhenya-kr <zhekakrochak79@gmail.com>
2026-05-23 09:26:48 +0300

    add js

---
+++ src/js/order-modal.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.modal-form');
+
+form.addEventListener('submit', handleSubmit);
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  const { username, phone, textComment } = event.target.elements;
+
+  const formData = {
+    name: username.value.trim(),
+    phone: phone.value.trim(),
+    dessertId: '6852a9fcb459460cb6b47748',
+    comment: textComment.value.trim(),
+  };
+
+  try {
+    const res = await axios.post(
+      'https://deserts-store.b.goit.study/api/orders',
+      formData
+    );
+
+    const orderData = res.data;
+    console.log(orderData);
+    iziToast.success({
+      title: `${orderData.name}`,
+      message: `Ви замовили ${orderData.dessertName}, номер вашого замовлення ${orderData.orderNum}`,
+      position: 'topRight',
+    });
+
+    event.target.reset();
+  } catch (error) {
+    iziToast.error({
+      title: 'Помилка',
+      message: `${error}`,
+      position: 'topRight',
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,2 @@
 import './js/dessert-list.js';
+import './js/order-modal.js';
