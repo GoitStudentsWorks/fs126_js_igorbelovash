acb6474d6971939cb6e02f39348206111f32adaf
Gennadiy Tsekhmistro <Tsekhmistro.gennadiy@gmail.com>
2026-05-23 15:14:21 +0300

    modified:   src/css/header.css         modified:   src/main.js         modified:   src/partials/header.html add src/img/menu.svg

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
 import './js/dessert-list.js';
+
+// burger menu
+const burger = document.getElementById("burgerBtn");
+const closeBtn = document.getElementById("closeBtn");
+
+const mobileMenu = document.getElementById("mobileMenu");
+
+const links = document.querySelectorAll(".mobile-link");
+
+// відкрити
+burger.addEventListener("click", () => {
+  mobileMenu.classList.add("open");
+});
+
+// закрити по X
+closeBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("open");
+});
+
+// закрити по кліку на пункт
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("open");
+  });
+});
