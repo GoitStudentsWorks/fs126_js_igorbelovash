d365706b15c8ed5c03c5edece5d7c213116453e1
Gennadiy Tsekhmistro <Tsekhmistro.gennadiy@gmail.com>
2026-05-23 15:48:26 +0300

    modified:   src/css/header.css        modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -3,24 +3,32 @@ import './js/dessert-list.js';
 // burger menu
 const burger = document.getElementById("burgerBtn");
 const closeBtn = document.getElementById("closeBtn");
-
 const mobileMenu = document.getElementById("mobileMenu");
-
 const links = document.querySelectorAll(".mobile-link");
 
 // відкрити
 burger.addEventListener("click", () => {
   mobileMenu.classList.add("open");
+  document.body.classList.add("menu-open");
 });
 
-// закрити по X
-closeBtn.addEventListener("click", () => {
+// закрити функція
+function closeMenu() {
   mobileMenu.classList.remove("open");
-});
+  document.body.classList.remove("menu-open");
+}
+
+// закрити по X
+closeBtn.addEventListener("click", closeMenu);
 
 // закрити по кліку на пункт
 links.forEach(link => {
-  link.addEventListener("click", () => {
-    mobileMenu.classList.remove("open");
-  });
+  link.addEventListener("click", closeMenu);
+});
+
+// закрити по ESC
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && mobileMenu.classList.contains("open")) {
+    closeMenu();
+  }
 });
