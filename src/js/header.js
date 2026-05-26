4a5282c58772edbd4c7f4167cf0df0d728776406
Nadja <nadja.pyvovar@gmail.com>
2026-05-26 14:49:52 +0200

    fix navbar-section

--- src/js/header.js
+++ src/js/header.js
@@ -1,32 +1,38 @@
 // burger menu
-const burger = document.getElementById("burgerBtn");
-const closeBtn = document.getElementById("closeBtn");
-const mobileMenu = document.getElementById("mobileMenu");
-const links = document.querySelectorAll(".mobile-link");
+const burger = document.getElementById('burgerBtn');
+const closeBtn = document.getElementById('closeBtn');
+const mobileMenu = document.getElementById('mobileMenu');
+const links = document.querySelectorAll('.mobile-link');
 
 // відкрити
-burger.addEventListener("click", () => {
-  mobileMenu.classList.add("open");
-  document.body.classList.add("menu-open");
+burger.addEventListener('click', () => {
+  mobileMenu.classList.add('open');
+  document.body.classList.add('menu-open');
+  burger.setAttribute('aria-expanded', 'true');
+
+  mobileMenu.setAttribute('aria-hidden', 'false');
 });
 
 // закрити функція
 function closeMenu() {
-  mobileMenu.classList.remove("open");
-  document.body.classList.remove("menu-open");
+  mobileMenu.classList.remove('open');
+  document.body.classList.remove('menu-open');
+  burger.setAttribute('aria-expanded', 'false');
+
+  mobileMenu.setAttribute('aria-hidden', 'true');
 }
 
 // закрити по X
-closeBtn.addEventListener("click", closeMenu);
+closeBtn.addEventListener('click', closeMenu);
 
 // закрити по кліку на пункт
 links.forEach(link => {
-  link.addEventListener("click", closeMenu);
+  link.addEventListener('click', closeMenu);
 });
 
 // закрити по ESC
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape" && mobileMenu.classList.contains("open")) {
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && mobileMenu.classList.contains('open')) {
     closeMenu();
   }
 });
